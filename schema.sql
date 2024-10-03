52ef54e4827814e2305fccb46d7bf8531c87e2ed
Eero Lumijärvi <eero.lumijarvi@helsinki.fi>
2024-10-03 17:34:51 +0300

    Added some more application base

--- schema.sql
+++ schema.sql
@@ -1,1 +1,3 @@
 CREATE TABLE visitors (id SERIAL PRIMARY KEY, time TIMESTAMP);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE reviews (id SERIAL PRIMARY KEY, username TEXT, review TEXT, stars INT);
