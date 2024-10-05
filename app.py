50fafef60e497f8ee53b294368cd9bf12a64b572
Eero Lumijärvi <eero@openSUSE.lan>
2024-10-06 02:41:36 +0300

    Modified app to add basic login functionality

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import redirect, render_template, request, session
 from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from sqlalchemy.sql import text
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 @app.route("/")
@@ -17,21 +18,40 @@ def index():
 def login():
     username = request.form["username"]
     password = request.form["password"]
-    # TODO: check username and password
-    sql = "SELECT id, password FROM users WHERE username=:username"
+    sql = text("SELECT id, password FROM users WHERE username=:username")
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()    
     if not user:
-        return "Not found", 400
+        session["notfound"] = username
+        return redirect("/")
     else:
         hash_value = user.password
         if check_password_hash(hash_value, password):
+            del session["notfound"] 
             session["username"] = username
             return redirect("/")
         else:
-            return "Not found", 400
+            session["notfound"] = username
+            return redirect("/")
+
 
 @app.route("/logout")
 def logout():
     del session["username"]
     return redirect("/")
+
+
+@app.route("/newUser")
+def new():
+    return render_template("newUser.html")
+
+@app.route("/createUser", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password = request.form["password"]
+    hash_value = generate_password_hash(password)
+    sql = text("INSERT INTO users (username, password) VALUES (:username, :password)")
+    db.session.execute(sql, {"username":username, "password":hash_value})
+    db.session.commit()
+    return redirect("/")
+
