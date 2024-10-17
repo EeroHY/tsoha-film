b5e2c51b92c81e7a426220d4d19ed6341a2ff372
Eero <eero.lumijarvi@helsinki.fi>
2024-10-18 02:00:40 +0300

    Added new templates, moved user functions to a seperate file

--- routes.py
+++ routes.py
@@ -3,54 +3,55 @@ from flask import render_template, request, redirect, session
 from os import getenv
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.sql import text
-from db import db
+import users
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
-
-@app.route("/login_notfound")
-def login_notfound():
-    return render_template("login.html", notfound=True)
-
-@app.route("/login_user",methods=["POST"])
-def login_user():
-    username = request.form["username"]
-    password = request.form["password"]
-    sql = text("SELECT id, password FROM users WHERE username=:username")
-    result = db.session.execute(sql, {"username":username})
-    user = result.fetchone()    
-    if not user:
-        return redirect("/login_notfound")
-    else:
-        hash_value = user.password
-        if check_password_hash(hash_value, password):
-            session["username"] = username
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
             return redirect("/")
         else:
-            return redirect("/login_notfound")
+            return render_template("login.html", notfound=True)
 
 
 @app.route("/logout")
 def logout():
-    del session["username"]
+    users.logout()
     return redirect("/")
 
-
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
 
-@app.route("/create_user", methods=["POST"])
-def create():
-    username = request.form["username"]
-    password = request.form["password"]
-    hash_value = generate_password_hash(password)
-    sql = text("INSERT INTO users (username, password) VALUES (:username, :password)")
-    db.session.execute(sql, {"username":username, "password":hash_value})
-    db.session.commit()
-    return redirect("/")
+        if password1 != password2:
+            return render_template("error.html", message="Passwords are different")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Failed to register user")
+
+@app.route("/profile", methods=["GET"])
+def profile():
+    return render_template("profile.html")
+
+#@app.route("/edit_user_name", methods=["POST"])
+#def edit_user_name():
+#    if request.method == "POST":
+#        username = request.form["username"]
+#        if users.register(username):
+#            return redirect("/profile")
+#        else:
+#            return render_template("error.html", message="Failed to change username")
